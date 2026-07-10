448ef93a6c58518d5940911f4fc6704f85c021f3
Elreyes009 <diegoreyessalvo2@gmail.com>
2026-07-10 16:43:07 -0400

    2

--- astro.config.ts
+++ astro.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 		mdx(),
 		sitemap(),
 		spectre({
-			name: "Diego Reyes",
+			name: "Kajir",
 			openGraph: {
 				home: { title: "Kajir", description: "Mi Portafolio" },
 				blog: { title: "3D Models", description: "Mi 3D models I have mede until now." },
